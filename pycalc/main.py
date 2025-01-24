4195d5de9fe44252589fe55e52f19bae33a3808e
vihrdaya <kevin.rajkumar@outlook.com>
2025-01-24 17:12:31 -0500

    added padbutton

--- pycalc/main.py
+++ pycalc/main.py
@@ -3,20 +3,27 @@ import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
 
+class PadButton(QtWidgets.QWidget):
+    def __init__(self, value):
+        super().__init__()
+
+        self.button = QtWidgets.QPushButton(f"{value}")
+
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         self.hello = ["Hello"]
 
-        self.button = QtWidgets.QPushButton("Click Me")
-        self.text = QtWidgets.QLabel("This is a Label", alignment = QtCore.Qt.AlignCenter)
-        
+        self.pad_button = PadButton("1")
+        self.text = QtWidgets.QLabel("This is a Label", alignment=QtCore.Qt.AlignCenter)
+
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.pad_button.button)
 
-        self.button.clicked.connect(self.magic)
+        self.pad_button.button.clicked.connect(self.magic)
 
     @QtCore.Slot()
     def magic(self):
@@ -27,7 +34,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(800,600)
+    widget.resize(800, 600)
     widget.show()
 
     sys.exit(app.exec())
