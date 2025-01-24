a117a96ec9a4001d207ccdd55ffcd439e5165f13
kevin <kevin.rajkumar@outlook.com>
2025-01-24 16:25:07 -0500

    Added a Qwidget

--- pycalc/main.py
+++ pycalc/main.py
@@ -1,3 +1,33 @@
 import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
+
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hello"]
+
+        self.button = QtWidgets.QPushButton("Click Me")
+        self.text = QtWidgets.QLabel("This is a Label", alignment = QtCore.Qt.AlignCenter)
+        
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800,600)
+    widget.show()
+
+    sys.exit(app.exec())
