12face8bfb9db8315a158fe4af0c05a4bd480e8d
vihrdaya <kevin.rajkumar@outlook.com>
2025-01-25 16:15:20 -0500

    added loop

--- pycalc/main.py
+++ pycalc/main.py
@@ -7,8 +7,6 @@ class PadButton(QtWidgets.QWidget):
     def __init__(self, value):
         super().__init__()
 
-        self.button = QtWidgets.QPushButton(f"{value}")
-
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -16,18 +14,21 @@ class MyWidget(QtWidgets.QWidget):
 
         self.hello = ["Hello"]
         self.num_1 = "1"
-        self.pad_button_1 = PadButton(f"{self.num_1}")
+        # self.pad_button_1 = PadButton(f"{self.num_1}")
         self.text = QtWidgets.QLabel("This is a Label", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.addButton(self.pad_button_1)
-        # self.layout.addWidget(self.pad_button_1.button)
+        self.addButton(self.num_1)
+        for i in range(10):
+            self.num = str(i)
+            self.addButton(self.num)
 
     @QtCore.Slot()
-    def addButton(self, pad_button):
-        self.layout.addWidget(pad_button.button)
-        pad_button.button.clicked.connect(self.magic)
+    def addButton(self, value):
+        pad_button = QtWidgets.QPushButton(f"{value}")
+        self.layout.addWidget(pad_button)
+        pad_button.clicked.connect(self.magic)
 
     @QtCore.Slot()
     def magic(self):
