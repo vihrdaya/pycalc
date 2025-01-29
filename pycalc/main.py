d7cb163b23ce4e019f0017309aa03239d85f48d5
kevin <kevin.rajkumar@outlook.com>
2025-01-28 22:39:48 -0500

    added button individuality

--- pycalc/main.py
+++ pycalc/main.py
@@ -13,26 +13,28 @@ class MyWidget(QtWidgets.QWidget):
         super().__init__()
 
         self.hello = ["Hello"]
-        self.num_1 = "1"
+        # self.num_1 = "1"
         # self.pad_button_1 = PadButton(f"{self.num_1}")
         self.text = QtWidgets.QLabel("This is a Label", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
-        self.addButton(self.num_1)
+        self.buttons = []
+        # self.addButton(self.num_1)
         for i in range(10):
-            self.num = str(i)
-            self.addButton(self.num)
+            str(i)
+            self.addButton(str(i))
 
     @QtCore.Slot()
     def addButton(self, value):
         pad_button = QtWidgets.QPushButton(f"{value}")
         self.layout.addWidget(pad_button)
-        pad_button.clicked.connect(self.magic)
+        self.buttons.append(pad_button)
+        pad_button.clicked.connect(lambda _, v=value: self.magic(v))
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.num_1))
+    def magic(self, value):
+        self.text.setText(random.choice(value))
 
 
 if __name__ == "__main__":
